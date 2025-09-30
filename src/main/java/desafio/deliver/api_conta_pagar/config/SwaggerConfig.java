44c5608642d24204221383926ba76d962b4a30f6
keysson Gomes <104814645+KeyssonG@users.noreply.github.com>
2025-09-30 08:07:27 -0300

    swagger

---
+++ src/main/java/desafio/deliver/api_conta_pagar/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package desafio.deliver.api_conta_pagar.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Gestão de Contas a pagar")
+                        .version("1.0.0")
+                        .description("API para gestão da Contas."));
+    }
+}
